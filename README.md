6b08e667ee6fd88c509e8e7361d6fca6b67aa30c
Mia Borja <apk.borja@gmail.com>
2022-07-18 15:27:55 -0700

    fixed generation errors

--- README.md
+++ README.md
@@ -1,13 +1,27 @@
 # Team Profile Generator 
 
-## About the Project
+## Description
 
-## Features
+This is a light-weight profile generator that utilizes the Inquirer.js UI to create a static HTML page of a team through the command line. There is a total of four different roles, which can be used more than once in the answering process.
 
-## Tools
-* Node.js
-* Jest.js
-* Inquirer.js
-* npm
+The HTML and CSS are both premade and only visibly accessible after finishing the Inquirer process.
 
-## Screenshots
+## Usage
+
+`npm i`
+
+`node index.js`
+
+## Bugs
+
+Due to an mapping error, it is advised to go in order of Manager -> Intern or Engineer. Any other order or running the add Manager command after the latter will cause the HTML to be formatted incorrectly and make the cards nest within each other.
+
+## Testing
+
+All testing of the classes was handled through **Jest**. There is a total of 9 tests across all of the team classes, and can be accessed and used through the command:
+
+`npm run test`
+
+Make sure to have run `npm i` before attempting to run the command.
+
+## Documentation
--- index.js
+++ index.js
@@ -1,26 +1,197 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const path = require('path');
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
+const Engineer = require('./lib/Engineer');
+
+const team = [];
 
 function init() {
     console.log('Welcome to the generator.');
 
-    beginQuestions();
+    beginGen();
 }
 
-inquirer
+function beginGen() {
+    inquirer
     .prompt([
         {
-            
-        }
-
+            type: 'list',
+            name: 'starter',
+            message: 'What team member would you like to add?',
+            choices: ['Add Manager', 'Add Intern', 'Add Engineer', 'Quit'],
+        },
     ])
-    .then (function (data) {
+    .then ((data) => {
+        if (data.starter === 'Add Manager') {
+            addManager();
+        } else if (data.starter === 'Add Intern') {
+            addIntern();
+        } else if (data.starter === 'Add Engineer') {
+            addEngineer();
+        } else {
+            console.log('Goodbye.');
+            pageBuilder(team);
+        }
+    });
+};
 
-    })
+function addManager() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: "What is your manager's name?",
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "What is your manager's ID?",
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "What is your manager's email?",
+            },
+            {
+                type: 'input',
+                name: 'officeNumber',
+                message: "What is your manager's office number?",
+            },
+        ])
+        .then((data) => {
+            const manager = new Manager(data.name, data.id, data.email, data.officeNumber);
+
+            team.push(manager);
+            // createRoleCard(manager);
+            beginGen();
+        })
+};
+
+function addIntern() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: "What is your intern's name?",
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "What is your intern's ID?",
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "What is your intern's email?",
+            },
+            {
+                type: 'input',
+                name: 'school',
+                message: "What is your intern's school?",
+            },
+        ])
+        .then((data) => {
+            const intern = new Intern(data.name, data.id, data.email, data.school);
+
+            team.push(intern);
+            // createRoleCard(intern);
+            beginGen();
+        })
+};
+
+function addEngineer() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: "What is your engineer's name?",
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "What is your engineer's ID?",
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "What is your engineer's email?",
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: "What is your engineer's Github?",
+            },
+        ])
+        .then((data) => {
+            const engineer = new Engineer(data.name, data.id, data.email, data.github);
+
+            team.push(engineer);
+            // createRoleCard(engineer);
+            beginGen();
+        })
+};
 
 function pageBuilder() {
-`
+    const employeeCards = team.map(employee => {
+        const role = employee.getRole();
+
+        if (role === 'Manager') {
+            return `<div class="container">
+            <div class="card">
+                <div class="card-header">
+                    <h1>${employee.name}</h1>
+                    <h2><i class="fa-solid fa-mug-hot"></i> Manager</h2>
+                </div>
+    
+                <div class="card-info">
+                    <ul>
+                        <li>ID: ${employee.id}</li>
+                        <li>Email: <a href='mailto:${employee.email}'>${employee.email}</a></li>
+                        <li>Office Number: ${employee.officeNumber}</li>
+                    </ul>
+            </div>
+            </div>`
+        } else if (role === 'Intern') {
+            return `<div class="card">
+                <div class="card-header">
+                    <h1>${employee.name}</h1>
+                    <h2><i class="fa-solid fa-user-graduate"></i> Intern</h2>
+                </div>
+    
+                <div class="card-info">
+                    <ul>
+                        <li>ID: ${employee.id}</li>
+                        <li>Email: <a href='mailto:${employee.email}'>${employee.email}</a></li>
+                        <li>School: ${employee.school}</li>
+                    </ul>
+                </div>
+            </div>`
+        } else if (role === 'Engineer') {
+            return `<div class="card">
+                <div class="card-header">
+                    <h1>${employee.name}</h1>
+                    <h2><i class="fa-solid fa-glasses"></i> Engineer</h2>
+                </div>
+    
+                <div class="card-info">
+                    <ul>
+                    <li>ID: ${employee.id}</li>
+                    <li>Email: <a href='mailto:${employee.email}'>${employee.email}</a></li>
+                        <li>GitHub: <a href="https://github.com/${employee.github}">${employee.github}</a></li>
+                    </ul>
+                </div>
+            </div>`
+        } else {
+            return;
+        }
+    }).join('');
+
+
+const pageHtml = `
 <!DOCTYPE html>
 
 <html lang="en">
@@ -37,14 +208,16 @@ function pageBuilder() {
         <header>
             <h1>MY TEAM</h1>
         </header>
-            
-        <div class="container">
-            
-        </div>
+
+        ${employeeCards}
     </body>
 
 </html>
-`
-}
+`;
+
+    fs.writeFile('./dist/index.html', pageHtml, function(err) {
+        err ? console.log(err) : console.log("Your team is finished, have a look?");
+    })
+};
 
 init();
