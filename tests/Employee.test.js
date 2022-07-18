9740749c9cdb60d27f8d959b08947dce9789cd1d
Mia Borja <apk.borja@gmail.com>
2022-07-18 03:03:10 -0700

    created tests for engineer and employee

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,5 +1,37 @@
 const Employee = require('../lib/Employee');
 
 describe("Employee", () => {
-    describe()
+    describe("Init", () => {
+        it("should return the name of the employee", () => {
+            const employed = new Employee('Jane', 3, 'janedoe@emailprovider.com');
+
+            const testEm = employed.getName();
+
+            expect(testEm).toEqual('Jane');
+        });
+
+        it("should return the id of the employee", () => {
+            const employed = new Employee('Jane', 3, 'janedoe@emailprovider.com');
+
+            const testEm = employed.getId();
+
+            expect(testEm).toEqual(3);
+        });
+
+        it("should return the email of the employee", () => {
+            const employed = new Employee('Jane', 3, 'janedoe@emailprovider.com');
+
+            const testEm = employed.getEmail();
+
+            expect(testEm).toEqual('janedoe@emailprovider.com');
+        });
+
+        it("should return the role of the employee", () => {
+            const employed = new Employee('Jane', 3, 'janedoe@emailprovider.com');
+
+            const testEm = employed.getRole();
+
+            expect(testEm).toEqual('Employee')
+        });
+    });
 });
--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -1,5 +1,20 @@
 const Engineer = require('../lib/Engineer');
 
-describe('Engineer', () => {
-    describe()
+describe("Engineer", () => {
+    describe("Init", () => {
+        it("should return the github username of the employee", () => {
+            const employed = new Emgineer('Jane', 3, 'janedoe@emailprovider.com', 'janesgithub');
+
+            const testEm = employed.getGithub();
+
+            expect(testEm).toEqual('janesgithub');
+        });
+        it("should return the role of the employee", () => {
+            const employed = new Emgineer('Jane', 3, 'janedoe@emailprovider.com', 'janesgithub');
+
+            const testEm = employed.getRole();
+
+            expect(testEm).toEqual('Engineer');
+        });
+    });
 });
