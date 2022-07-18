ecc7d18c309ba432f40a01b2984ea00e84a86be7
Mia Borja <apk.borja@gmail.com>
2022-07-18 02:32:50 -0700

    updated employee class

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+
+function init() {
+    console.log('Welcome to the generator.');
+
+    beginQuestions();
+}
+
 inquirer
     .prompt([
         {
@@ -9,4 +19,32 @@ inquirer
 
     })
 
-module.exports = g;
+function pageBuilder() {
+`
+<!DOCTYPE html>
+
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1" /> 
+        <title>Team Profile</title>
+        <link rel="stylesheet" type="text/css" href="./style.css" />
+        <script src="https://kit.fontawesome.com/5efd198a8f.js" crossorigin="anonymous"></script>
+    </head>
+
+    <body>
+        <header>
+            <h1>MY TEAM</h1>
+        </header>
+
+        <div class="container">
+            
+        </div>
+    </body>
+
+</html>
+`
+}
+
+init();
